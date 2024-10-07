f5b0dc5132278809745a84b77037277beafa0e07
Durgesh Kulkarni <66022213+durgeshkulkarni1999@users.noreply.github.com>
2024-10-07 15:34:21 -0400

    Update footer.tsx

--- components/footer.tsx
+++ components/footer.tsx
@@ -6,7 +6,7 @@ export default function Footer() {
   return (
     <footer className="mb-10 px-4 text-center text-gray-500">
       <small className="mb-2 block text-xs">
-       &copy; {year} Akash's Portfolio. 
+       &copy; {year} Durgesh's Portfolio. 
       </small>
       <p className="text-xs">
         <span className="font-semibold">About this website:</span> built with
